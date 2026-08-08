428dcead46c4a9eb35c1afac23177bbd22b896f0
yanas-logs <81765889+yanas-logs@users.noreply.github.com>
2026-08-08 23:30:20 +0700

    Create include/wikicpp/wikicpp.hpp

---
+++ include/wikicpp/wikicpp.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include
+
+#define WIKICPP_VERSION_MAJOR 0
+#define WIKICPP_VERSION_MINOR 1
+#define WIKICPP_VERSION_PATCH 0
+#define WIKICPP_VERSION "0.1.0"
+
+namespace wikicpp {
+}
